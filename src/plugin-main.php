a64441af6aa71d6d2e95edc01e529360cdd24d65
daigo75 <daigo75@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-02-08 20:56:55 +0000

    Fixed formatting in readme.txt
    git-svn-id: https://plugins.svn.wordpress.org/woocommerce-skrill-moneybookers-gateway/trunk@1085136 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- src/plugin-main.php
+++ src/plugin-main.php
@@ -18,7 +18,7 @@ use \WC_Gateway_Skrill;
  */
 class WC_Skrill_Gateway_Plugin extends Aelia_Plugin {
 	// @var string The plugin version
-	public static $version = '1.2.17.150208';
+	public static $version = '1.2.18.150208';
 
 	public static $plugin_slug = Definitions::PLUGIN_SLUG;
 	public static $text_domain = Definitions::TEXT_DOMAIN;
